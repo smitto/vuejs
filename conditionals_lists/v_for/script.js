93e9358684750ec0fab1d653e20a82ff467a0842
Nathan Smith <nathansforyou@gmail.com>
2019-10-30 00:13:49 -0400

    Add conditional and list module

---
+++ conditionals_lists/v_for/script.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'potatoes', 'iodine'],
+    persons: [
+      {name: 'Big', age: 420, color: 'red'},
+      {name: 'Large', age: 69, color: 'blue'}
+    ]
+  }
+});
