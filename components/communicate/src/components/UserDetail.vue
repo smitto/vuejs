8fd387b79e4e22bdb1d0dd3ea9882faa8f3dabe5
Nathan Smith <nathansforyou@gmail.com>
2019-11-19 00:12:27 -0500

    component communication

---
+++ components/communicate/src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ myName }}</p>
+        <p>Reversed User Name: {{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: {
+                type: String,
+                default: 'Nathan'
+            }
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
